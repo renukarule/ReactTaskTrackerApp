393b9eae73554f5382dfb6b78429434b236ee9bc
renukarule <90004218+renukarule@users.noreply.github.com>
2023-07-05 21:29:16 -0700

    Add files via upload

---
+++ About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h4>Version 1.0.0</h4>
+    </div>
+  )
+}
+
+export default About
---
+++ Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <p> Copyright &Copy; 2023</p>
+        <a href='/about'>About</a>
+      
+    </footer>
+  )
+}
+
+export default Footer
